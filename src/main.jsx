528ac3363e4d6e89cbf478b33d33696f6a3e5c0e
HafizSyedUsamaBinQamar <usamedevio@gmail.com>
2023-12-09 01:10:42 +0500

    q
    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
@@ -7,3 +8,13 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <App />
    </React.StrictMode>
 );
+=======
+// src/main.js
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
+>>>>>>> 2c32e6a (initial commit)
